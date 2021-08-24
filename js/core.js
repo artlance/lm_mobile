955519857ee146f7c2d4f3b1e0eae27f967fc51f
artlance <artlance.ru@gmail.com>
2021-08-24 20:51:27 +0300

    товар в шоуруме + окно

--- js/core.js
+++ js/core.js
@@ -451,6 +451,9 @@ $(document).ready(function () {
                         centerInsufficientSlides: true,
                     });
                 }
+                if ($('.phone-mask').length) {
+                    $('.phone-mask').mask('+7 (999) 999-99-99');
+                }
             }
         });
     }
